0295118b3b3b144cd3ab21aec21338d9bf2d068e
SergVLu <rumasyba@gmail.com>
2024-12-12 01:07:31 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>lc41</title>
+</head>
+<body>
+  <h1>LC-41</h1>
+  <h2>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, quaerat ullam minima quos iusto, sed ipsa nam sequi dolores delectus at? Quam, placeat iure. Velit neque et reprehenderit itaque ut.
+  </h2>  
+</body>
+</html>
